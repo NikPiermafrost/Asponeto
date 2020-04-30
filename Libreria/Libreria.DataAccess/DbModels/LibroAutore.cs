75c6338e3d882df706efc0c6c726fe74d7c784a4
Nicola Piermattei <pierma.nick@live.it>
2020-04-30 22:14:02 +0200

    Aggiunti dbset

---
+++ Libreria/Libreria.DataAccess/DbModels/LibroAutore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Libreria.DataAccess.DbModels
+{
+    public class LibroAutore
+    {
+        [Key]
+        public int LibroAutoreId { get; set; }
+        public int LibroId { get; set; }
+        public int AutoreId { get; set; }
+        public virtual Libro Libro { get; set; }
+        public virtual Autore Autore { get; set; }
+    }
+}
---
+++ Libreria/Libreria.DataAccess/LibreriaContext.cs
@@ -0,0 +1,16 @@
+﻿using Libreria.DataAccess.DbModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Libreria.DataAccess
+{
+    public class LibreriaContext: DbContext
+    {
+        public LibreriaContext(DbContextOptions<LibreriaContext> options): base(options) { }
+        public DbSet<Persona> Personas { get; set; }
+        public DbSet<Libro> Libros { get; set; }
+        public DbSet<Libreriaa> Librerias { get; set; }
+        public DbSet<LibroAutore> LibroAutores { get; set; }
+        public DbSet<Autore> Autores { get; set; }
+    }
+}
